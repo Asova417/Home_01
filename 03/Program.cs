a01aa33f5c073703a018d92e48045c67fa43dfd6
SpanglMindCrow <asova417@gmail.com>
2022-04-16 19:49:19 +0500

    add Task 3.

---
+++ 03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int  numberA = Convert.ToInt32(Console.ReadLine());
+double  numberB = numberA;
+int a = ((numberA)/2);
+double b = (numberB)/2;
+
+if (a == b)
+{
+    Console.WriteLine("Ответ: чётное");
+}
+if (a > b)
+{
+Console.WriteLine("Ответ: не чётное");
+}
+if (a < b)
+{
+Console.WriteLine("Ответ: не чётное");
+}
