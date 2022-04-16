f489f8371116032f002d9af82ca806b011a2a878
SpanglMindCrow <asova417@gmail.com>
2022-04-16 18:30:12 +0500

    add Task 1.

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Доброго времени суток!");
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
+if (a < b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.WriteLine(a); 
+}
+if (a==b)
+{
+     Console.WriteLine("Все фигня, они равны!"); 
+}
