04716a7b8409f5a152b1f3065c99119e44918c87
Asova417 <asova417@gmail.com>
2022-04-17 19:57:31 +0500

    add Task 7

--- 07/Program.cs
+++ 07/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a>100)
+{
+    a=a-100;
+}
+while (a>10)
+{
+    a=a-10;
+}
+if (a<10)
+{
+    Console.WriteLine(a);
+}
